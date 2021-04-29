ffd2b9aeb850f4afc375f9e38898b365557ec2af
jt <47215199+tvrjuana@users.noreply.github.com>
2021-04-29 15:20:22 -0400

    file to connect to database
    change my username to yours

---
+++ connect.php
@@ -0,0 +1,9 @@
+<?php
+$con = mysqli_connect("localhost","jtavera1","jtavera1","jtavera1");
+
+// Check connection
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  exit();
+}
+?>
