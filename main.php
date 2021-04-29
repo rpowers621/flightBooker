4db12bd2a41b32dc1649786426ce55320bec412c
rpowers621 <61554069+rpowers621@users.noreply.github.com>
2021-04-29 18:21:26 -0400

    created main page

---
+++ main.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+    <title></title>
+  </head>
+  <body>
+    <div class="container" style="min-width: 1000px;">
+      <div class="header">
+        GSU Airlines
+        <button type="button" name="book-park">Book Parking</button>
+        <button type="button" name="signup">Sign Up</button>
+        <button type="button" name="login">Login</button>
+        <button type="button" name="cart">Cart</button>
+        //user circle
+      </div>
+      <div class="flight-control">
+
+        <form class="" action="index.html" method="post">
+          <select class="" name="flight-from" value="From">
+            <option value="ATL">ATL</option>
+            <option value="MIA">MIA</option>
+            <option value="PHL">PHL</option>
+            <option value="JFK">JFK</option>
+            <option value="LAX">LAX</option>
+          </select>
+
+          &nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;&nbsp;
+          &larr; &rarr;
+          &nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;&nbsp;
+
+          <select class="" name="flight-to" value="To">
+            <option value="ATL">ATL</option>
+            <option value="MIA">MIA</option>
+            <option value="PHL">PHL</option>
+            <option value="JFK">JFK</option>
+            <option value="LAX">LAX</option>
+          </select>
+
+          &nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;&nbsp;
+
+
+          <input type="date" name="date" value="2021-04-29" size="10">
+          <input type="submit" name="" value="Submit">
+        </form>
+
+      </div>
+
+      <div class="image-text" >
+        <img src="vacation.jpeg" style="width:100%; min-width:1000px">
+        <div class="img-text">
+
+            CATCH
+            <br>
+            FLIGHTS
+            <br>
+            NOT FEELINGS.
+
+        </div>
+      </div>
+
+    </div>
+
+  </body>
+</html>
