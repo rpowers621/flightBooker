e5b326cda03d3ac4420a9263616ee861a2d609a1
jt <47215199+tvrjuana@users.noreply.github.com>
2021-04-30 14:23:43 -0400

    inserting values - airplane seats, customers

--- insertValues.php
+++ insertValues.php
@@ -11,6 +11,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
+//when inserting values into database must do it one by one or can change the name of the variable to be all different and add the corresponding if statements
 $sql = "INSERT INTO inventory (rowNumber, seat, typeof, price)
 VALUES (1, 'A', 'First', 250.00)";
 
