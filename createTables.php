e67462aeac540460d566c4b4131058eae462d5f5
jt <47215199+tvrjuana@users.noreply.github.com>
2021-04-30 10:43:38 -0400

    Add files via upload

---
+++ createTables.php
@@ -0,0 +1,44 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "booking";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+// sql to create table
+/*$sql = "CREATE TABLE customer (
+	id INT(50) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	firstname VARCHAR(30) NOT NULL,
+	lastname VARCHAR(30) NOT NULL,
+	email VARCHAR(50)
+)";*/
+
+/*$sql = "CREATE TABLE inventory (
+	id INT(50) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	rowNumber INT(30) NOT NULL,
+	seat VARCHAR(30) NOT NULL
+)";*/
+
+$sql = "CREATE TABLE orders (
+	id INT(50) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	firstname VARCHAR(30) NOT NULL,
+	lastname VARCHAR(30) NOT NULL,
+	rowNumber INT(30) NOT NULL,
+	seat VARCHAR(30) NOT NULL,
+	email VARCHAR(50)
+)";
+
+if (mysqli_query($conn, $sql)) {
+  echo "Table created successfully";
+} else {
+  echo "Error creating table: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
