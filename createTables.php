7ffc9bd3f0a1111da68c91bed366a9984cd0431b
jt <47215199+tvrjuana@users.noreply.github.com>
2021-04-30 14:38:21 -0400

    added username column for customer

--- createTables.php
+++ createTables.php
@@ -18,6 +18,7 @@ $sql = "CREATE TABLE customer (
 	firstname VARCHAR(30) NOT NULL,
 	lastname VARCHAR(30) NOT NULL,
 	email VARCHAR(50) NOT NULL,
+	username VARCHAR(30) NOT NULL, 
 	passwords VARCHAR(30) NOT NULL
 )";
 
